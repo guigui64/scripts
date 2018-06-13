34ef0c99b952961c7b49b1d83340128af69fefec
Guillaume Comte <guillaume.comte@airbus.com>
2018-06-13 14:59:24 +0200

    Cache dependencies

--- envSimTG.sh
+++ envSimTG.sh
@@ -6,15 +6,38 @@ TARTGET="Ubuntu_16.04/64bit"
 TARGET_DEP="Ubuntu_16.04/64bit"
 
 # Dependencies Locations
-export ACE_ROOT=/tools/dependencies/${TARGET_DEP}/ACE-TAO/6.1.0
-export TAO_ROOT=/tools/dependencies/${TARGET_DEP}/ACE-TAO/6.1.0
-export ZMQ_ROOT=/tools/dependencies/${TARGET_DEP}/zeromq/4.0.3
-#export XERCESCROOT=/tools/dependencies/${TARGET_DEP}/xerces-c/2.8.0
-export XERCESCROOT=/usr
+DEPS_CACHE=${HOME}/tmp/DEPS_CACHE
+# export ACE_ROOT=${SDK_PATH}/tools/dependencies/${TARGET_DEP}/ACE-TAO/6.1.0
+if [ ! -e ${DEPS_CACHE}/ACE-TAO/6.1.0 ] ; then
+    echo "Caching ACE-TAO 6.1.0..."
+    mkdir -p ${DEPS_CACHE}/ACE-TAO
+    cp -r ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/ACE-TAO/6.1.0/ ${DEPS_CACHE}/ACE-TAO/
+fi
+export ACE_ROOT=${DEPS_CACHE}/ACE-TAO/6.1.0
+export TAO_ROOT=${ACE_ROOT}
+# export XERCESCROOT=${SDK_PATH}/tools/dependencies/${TARGET_DEP}/xerces-c/2.8.0
+if [ ! -e ${DEPS_CACHE}/xerces-c/2.8.0 ] ; then
+    echo "Caching xerces-c 2.8.0..."
+    mkdir -p ${DEPS_CACHE}/xerces-c
+    cp -r ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/xerces-c/2.8.0/ ${DEPS_CACHE}/xerces-c/
+fi
+export XERCESCROOT=${DEPS_CACHE}/xerces-c/2.8.0
 export XERCES_ROOT=${XERCESCROOT}
-export TSP_ROOT=/tools/dependencies/${TARGET_DEP}/tsp/0.8.3
+# export ZMQ_ROOT=${SDK_PATH}/tools/dependencies/${TARGET_DEP}/zeromq/4.0.3
+if [ ! -e ${DEPS_CACHE}/zeromq/4.0.3 ] ; then
+    echo "Caching zeromq 4.0.3..."
+    mkdir -p ${DEPS_CACHE}/zeromq
+    cp -r ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/zeromq/4.0.3/ ${DEPS_CACHE}/zeromq/
+fi
+export ZMQ_ROOT=${DEPS_CACHE}/zeromq/4.0.3
+# export TSP_ROOT=${SDK_PATH}/tools/dependencies/${TARGET_DEP}/tsp/0.8.3
+if [ ! -e ${DEPS_CACHE}/tsp/0.8.3 ] ; then
+    echo "Caching tsp 0.8.3..."
+    mkdir -p ${DEPS_CACHE}/tsp
+    cp -r ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/tsp/0.8.3/ ${DEPS_CACHE}/tsp/
+fi
+export TSP_ROOT=${DEPS_CACHE}/tsp/0.8.3
 export PYTHON_ROOT=/usr/bin
-export JAVA_HOME=/usr/java/default
 
 export SIMTG_INSTALL_ROOT=/tools/simtg
 export MODELS_INSTALL_ROOT=/models
