1e54ecf7e1b1f7f797001ed21e62caaa1586708b
Guillaume Comte <guillaume.comte@airbus.com>
2020-01-31 14:25:25 +0100

    envSimTG: rsync rather than cp

--- envSimTG.sh
+++ envSimTG.sh
@@ -2,57 +2,55 @@
 # This script should be sourced to set the environment used for cores etc.
 # This is inspired by the /usr/local/bin/eclipseIASI-NG script
 
-TARGET_DEP="Ubuntu_16.04/64bit"
+TARGET_DEP="Ubuntu_18.04/64bit"
+GEN_TARGET_DEP="Linux/64bit"
 
 # Dependencies Locations
 DEPS_CACHE=${HOME}/tmp/DEPS_CACHE
-# export ACE_ROOT=${SDK_PATH}/tools/dependencies/${TARGET_DEP}/ACE-TAO/6.1.0
-if [ ! -e ${DEPS_CACHE}/ACE-TAO/6.1.0 ] ; then
-	echo "Caching ACE-TAO 6.1.0..."
-	mkdir -p ${DEPS_CACHE}/ACE-TAO
-	cp -r ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/ACE-TAO/6.1.0/ ${DEPS_CACHE}/ACE-TAO/
+# ACE-TAO
+if [ ! -e ${DEPS_CACHE}/${TARGET_DEP}/ACE-TAO/6.4.8 ] ; then
+	echo "Caching ACE-TAO 6.4.8..."
+	mkdir -p ${DEPS_CACHE}/${TARGET_DEP}/ACE-TAO
+	rsync --info=progress2 -az ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/ACE-TAO/6.4.8 ${DEPS_CACHE}/${TARGET_DEP}/ACE-TAO/
 fi
-export ACE_ROOT=${DEPS_CACHE}/ACE-TAO/6.1.0
+export ACE_ROOT=${DEPS_CACHE}/${TARGET_DEP}/ACE-TAO/6.4.8
 export TAO_ROOT=${ACE_ROOT}
-# export XERCESCROOT=${SDK_PATH}/tools/dependencies/${TARGET_DEP}/xerces-c/2.8.0
-if [ ! -e ${DEPS_CACHE}/xerces-c/2.8.0 ] ; then
-	echo "Caching xerces-c 2.8.0..."
-	mkdir -p ${DEPS_CACHE}/xerces-c
-	cp -r ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/xerces-c/2.8.0/ ${DEPS_CACHE}/xerces-c/
-fi
-export XERCESCROOT=${DEPS_CACHE}/xerces-c/2.8.0
-export XERCES_ROOT=${XERCESCROOT}
-if [ ! -e ${DEPS_CACHE}/xerces-c/3.1.2 ] ; then
+# XERCES
+# if [ ! -e ${DEPS_CACHE}/${TARGET_DEP}/xerces-c/2.8.0 ] ; then
+# 	echo "Caching xerces-c 2.8.0..."
+# 	mkdir -p ${DEPS_CACHE}/${TARGET_DEP}/xerces-c
+# 	rsync --info=progress2 -az ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/xerces-c/2.8.0 ${DEPS_CACHE}/${TARGET_DEP}/xerces-c/
+# fi
+if [ ! -e ${DEPS_CACHE}/${TARGET_DEP}/xerces-c/3.1.2 ] ; then
 	echo "Caching xerces-c 3.1.2..."
-	mkdir -p ${DEPS_CACHE}/xerces-c
-	cp -r ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/xerces-c/3.1.2/ ${DEPS_CACHE}/xerces-c/
+	mkdir -p ${DEPS_CACHE}/${TARGET_DEP}/xerces-c
+	rsync --info=progress2 -az ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/xerces-c/3.1.2 ${DEPS_CACHE}/${TARGET_DEP}/xerces-c/
 fi
-# export ZMQ_ROOT=${SDK_PATH}/tools/dependencies/${TARGET_DEP}/zeromq/4.0.3
-if [ ! -e ${DEPS_CACHE}/zeromq/4.0.3 ] ; then
+export XERCESCROOT=${DEPS_CACHE}/${TARGET_DEP}/xerces-c/3.1.2
+export XERCES_ROOT=${XERCESCROOT}
+# ZMQ
+if [ ! -e ${DEPS_CACHE}/${TARGET_DEP}/zeromq/4.0.3 ] ; then
 	echo "Caching zeromq 4.0.3..."
-	mkdir -p ${DEPS_CACHE}/zeromq
-	cp -r ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/zeromq/4.0.3/ ${DEPS_CACHE}/zeromq/
+	mkdir -p ${DEPS_CACHE}/${TARGET_DEP}/zeromq
+	rsync --info=progress2 -az ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/zeromq/4.0.3 ${DEPS_CACHE}/${TARGET_DEP}/zeromq/
 fi
-export ZMQ_ROOT=${DEPS_CACHE}/zeromq/4.0.3
-# export TSP_ROOT=${SDK_PATH}/tools/dependencies/${TARGET_DEP}/tsp/0.8.3
-if [ ! -e ${DEPS_CACHE}/tsp/0.8.3 ] ; then
-	echo "Caching tsp 0.8.3..."
-	mkdir -p ${DEPS_CACHE}/tsp
-	cp -r ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/tsp/0.8.3/ ${DEPS_CACHE}/tsp/
-fi
-export TSP_ROOT=${DEPS_CACHE}/tsp/0.8.3
-if [ ! -e ${DEPS_CACHE}/xsd/4.0.0 ] ; then
+export ZMQ_ROOT=${DEPS_CACHE}/${TARGET_DEP}/zeromq/4.0.3
+# XSD
+if [ ! -e ${DEPS_CACHE}/${GEN_TARGET_DEP}/xsd/4.0.0 ] ; then
 	echo "Caching xsd 4.0.0..."
-	mkdir -p ${DEPS_CACHE}/xsd
-	cp -r ${SDK_PATH}/tools/dependencies/Linux/64bit/xsd/4.0.0/ ${DEPS_CACHE}/xsd
+	mkdir -p ${DEPS_CACHE}/${GEN_TARGET_DEP}/xsd
+	rsync --info=progress2 -az ${SDK_PATH}/tools/dependencies/${GEN_TARGET_DEP}/xsd/4.0.0 ${DEPS_CACHE}/${GEN_TARGET_DEP}/xsd
 fi
-export XSD_ROOT=${DEPS_CACHE}/xsd/4.0.0
-if [ ! -e ${DEPS_CACHE}/protobuf/3.0.0 ] ; then
+export XSD_ROOT=${DEPS_CACHE}/${GEN_TARGET_DEP}/xsd/4.0.0
+# PROTOBUF
+if [ ! -e ${DEPS_CACHE}/${TARGET_DEP}/protobuf/3.0.0 ] ; then
 	echo "Caching protobuf 3.0.0..."
-	mkdir -p ${DEPS_CACHE}/protobuf
-	cp -r ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/protobuf/3.0.0/ ${DEPS_CACHE}/protobuf
+	mkdir -p ${DEPS_CACHE}/${TARGET_DEP}/protobuf
+	rsync --info=progress2 -az ${SDK_PATH}/tools/dependencies/${TARGET_DEP}/protobuf/3.0.0 ${DEPS_CACHE}/${TARGET_DEP}/protobuf
 fi
-export PROTOBUF_ROOT=${DEPS_CACHE}/protobuf/3.0.0
+export PROTOBUF_ROOT=${DEPS_CACHE}/${TARGET_DEP}/protobuf/3.0.0
+
+# PYTHON
 export PYTHON_ROOT=/usr/bin
 
 export SIMTG_INSTALL_ROOT=/tools/simtg
@@ -64,7 +62,7 @@ export HYBRID_INSTALL_ROOT=/hybrid
 export PATH=${TAO_ROOT}/bin:${ACE_ROOT}/bin:${ZMQ_ROOT}/bin:${PATH}
 
 # Export LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=${TAO_ROOT}/lib:${ACE_ROOT}/lib:${XERCESCROOT}/lib:${TSP_ROOT}/lib:${XSD_ROOT}/lib:${PROTOBUF_ROOT}/lib:${ZMQ_ROOT}/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${TAO_ROOT}/lib:${ACE_ROOT}/lib:${XERCESCROOT}/lib:${XSD_ROOT}/lib:${PROTOBUF_ROOT}/lib:${ZMQ_ROOT}/lib:${LD_LIBRARY_PATH}
 
 # Set Workspace
 if [ ${WORKSPACE} ]
@@ -82,8 +80,8 @@ echo " SIMTG_INSTALL_ROOT    = ${SIMTG_INSTALL_ROOT}"
 echo " MODELS_INSTALL_ROOT   = ${MODELS_INSTALL_ROOT}"
 echo " PROJECTS_INSTALL_ROOT = ${PROJECTS_INSTALL_ROOT}"
 echo " ACE/TAO_ROOT          = ${ACE_ROOT}"
-echo " TSP_ROOT              = ${TSP_ROOT}"
 echo " XSD_ROOT              = ${XSD_ROOT}"
+echo " XERCES_ROOT           = ${XERCES_ROOT}"
 echo " PROTOBUF_ROOT         = ${PROTOBUF_ROOT}"
 echo " ZMQ_ROOT              = ${ZMQ_ROOT}"
 echo " JAVA_HOME             = ${JAVA_HOME}"
